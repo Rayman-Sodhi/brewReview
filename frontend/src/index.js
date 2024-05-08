7c3b723f550584312c07fc6e3ab625ed599d205c
Rayman-Sodhi <qaursodhi@gmail.com>
2024-05-09 01:10:35 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import moengage from "@moengage/web-sdk";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+moengage.initialize({app_id: '36EDJNDOCX822951NKI9VZ8T', cluster: 'DC_1'});
+moengage.add_email('abc@xyz.com')
+
 root.render(
   <React.StrictMode>
     <App />
